192eeeaf961a03df74c2561ea7c2deeb10663d3c
mauor <mauorozcoo@gmail.com>
2024-04-19 12:24:43 -0600

    Add email Service

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -8,7 +8,14 @@ export const envs = {
     
     MONGO_URL: get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+    
     JWT_SEED: get('JWT_SEED').required().asString(),
+
+    MAILER_SERVICE: get('MAILER_SERVICE').required().asString(),
+    MAILER_EMAIL: get('MAILER_EMAIL').required().asString(),
+    MAILER_KEY_SECRET: get('MAILER_KEY_SECRET').required().asString(),
+
+    WEBSERVICE_URL: get('WEBSERVICE_URL').required().asString(),
 
 }
 
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,11 +1,12 @@
-import { JwtAdapter, bcryptAdapter } from "../../config";
+import { EmailService } from './email.service';
+import { JwtAdapter, bcryptAdapter, envs } from "../../config";
 import { UserModel } from "../../data";
 import { CustomError, LoginUserDto, RegisterUserDto, UserEntity } from "../../domain";
 
 export class AuthService {
     
     constructor(
-        
+        private readonly emailService: EmailService,
     ){}
 
     public async registerUser( registerUserDto: RegisterUserDto ){
@@ -15,13 +16,20 @@ export class AuthService {
         try{
             const user = new UserModel( registerUserDto );
             user.password = bcryptAdapter.hash( registerUserDto.password );
+
             await user.save();
+
+            await this.sendEmailValidationLinck( user.email );
+
+            const token = await JwtAdapter.generateToken({ id: user.id });
+            if (!token) throw CustomError.internalServerError('Error generating token');
+
             const { password, ...userEntity} = UserEntity.fromObject(user);
             return {
                 user: { 
                     ...userEntity
                 }, 
-                token: 'TODO_TOKEN'
+                token
             };
         }
         catch(error){
@@ -38,7 +46,7 @@ export class AuthService {
         if (!validPassword) throw CustomError.badRequest('Incorrect password');
 
         const { password, ...userEntity } = UserEntity.fromObject( user );
-        const token = await JwtAdapter.generateToken( {id: user.id, email: user.email} );
+        const token = await JwtAdapter.generateToken( { id: user.id } );
         if(!token) throw CustomError.internalServerError('Error generating token');
         return{
             user: {
@@ -46,6 +54,28 @@ export class AuthService {
             },
             token
         }
+    }
+
+    private sendEmailValidationLinck = async (email: string) => {
+        const token = JwtAdapter.generateToken({email});
+        if (!token) throw CustomError.internalServerError('Error generating validation token');
+
+        const link = `${envs.WEBSERVICE_URL}/auth/validate-email/${token}`;
+
+        const html = `
+            <h1>Validate your email</h1>
+            <p>Click on the following link to validate your email<p>
+            <a href="${link}">Validate ${email}</a>
+        `;
+        const options = {
+            to: email,
+            subject: 'Validate your email',
+            htmlBody: html,
+        }
+        const isSent = await this.emailService.sendEmail( options );
+        if( !isSent ) throw CustomError.internalServerError('Error sending email');
+
+        return true;
     }
 
 }
